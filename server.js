dd6c456e55d362abf9f36c536470b7275f5ef863
corruptionbangla24-ops <corruptionbangla24@gmail.com>
2026-05-27 02:33:42 +0600

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
     next();
 });
 
-const MAIN_SITE_URL = "https://onrender.com"; 
+const MAIN_SITE_URL = "https://betlover247.onrender.com"; 
 
 // 🎡 [আপনার রিকোয়ারমেন্ট অনুযায়ী ৩টি ফিজিক্যাল আইটেম বাদে পিউর ২৪-ঘরের ওরিজিনাল ব্যাকএন্ড প্রফিট তালিকা ভাই ভাই]
 const prizeMatrix = [
