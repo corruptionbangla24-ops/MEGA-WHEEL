11f8b6bc7460c2e958593baba6f72da212768971
corruptionbangla24-ops <corruptionbangla24@gmail.com>
2026-05-26 12:59:07 +0600

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 });
 
 // 🎰 [উইনগো কালার ট্রেড ওরিজিনাল ডোমেইন সিঙ্ক]
-const MAIN_SITE_URL = "https://onrender.com"; 
+const MAIN_SITE_URL = "https://betlover247.onrender.com"; 
 
 // 🎡 চাকার ১২টি ঘরের ডাইনামিক রেট মেমোরি ইনডেক্স ভাই ভাই
 const prizeMatrix = [
